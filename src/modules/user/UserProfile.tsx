972d31e62b76c63455503b160766342dbb07035b
duydev245 <duyhtse182314@fpt.edu.vn>
2026-04-19 00:06:12 +0700

    user-profile

--- src/modules/user/UserProfile.tsx
+++ src/modules/user/UserProfile.tsx
@@ -1,4 +1,9 @@
+import { useParams } from 'react-router'
+
 function UserProfile() {
+  const { username } = useParams()
+  console.log('🚀 ~ UserProfile ~ username:', username)
+
   return <div>UserProfile</div>
 }
 
